1259d3d7cacb995cdc2493bd90f737bc2aa994d4
hanggerui88 <15834584868@163.com>
2025-10-15 09:41:39 +0800

    Site updated: 2025-10-15 09:41:38

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759670823852";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760492467125";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
